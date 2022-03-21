06395026919fcd85cb697890c0a821728f8051e2
smukher1 <smukher1@mazdausa.com>
2022-03-21 12:19:18 +0530

    Update ListController.java

--- MFPCR/src/main/java/com/mnao/mfp/list/controller/ListController.java
+++ MFPCR/src/main/java/com/mnao/mfp/list/controller/ListController.java
@@ -20,151 +20,153 @@ import java.util.List;
 @CrossOrigin(origins = "*")
 @RequestMapping("/Lists/")
 public class ListController extends MfpKPIControllerBase {
-    //
-    private static final Logger log = LoggerFactory.getLogger(ListController.class);
+	//
+	private static final Logger log = LoggerFactory.getLogger(ListController.class);
 
-    //
-    @PostMapping("/ListDealers")
-    public CommonResponse<List<ListDealer>> listDealers(@RequestParam(value = "rgnCd", defaultValue = "") String rgnCd,
-                                                        @RequestParam(value = "zoneCd", defaultValue = "") String zoneCd,
-                                                        @RequestParam(value = "districtCd", defaultValue = "") String districtCd,
-                                                        @RequestParam(value = "mdaCd", defaultValue = "") String mdaCd,
-                                                        @SessionAttribute(name = "mfpUser") MFPUser mfpUser) {
-        String sqlName = getKPIQueryFilePath(AppConstants.SQL_LIST_DEALERS);
-        ListService<ListDealer> service = new ListService<ListDealer>();
-        List<ListDealer> retRows = null;
-        DealerFilter df = new DealerFilter(mfpUser, null, rgnCd, zoneCd, districtCd, mdaCd);
-        try {
-            retRows = service.getListData(sqlName, ListDealer.class, df);
-        } catch (InstantiationException | IllegalAccessException | ParseException e) {
-            log.error("ERROR retrieving list of Dealers:", e);
-        }
-        return AbstractService.httpPostSuccess(retRows, "Success");
-    }
+	//
+	@PostMapping("/ListDealers")
+	public CommonResponse<List<ListDealer>> listDealers(@RequestParam(value = "rgnCd", defaultValue = "") String rgnCd,
+			@RequestParam(value = "zoneCd", defaultValue = "") String zoneCd,
+			@RequestParam(value = "districtCd", defaultValue = "") String districtCd,
+			@RequestParam(value = "mdaCd", defaultValue = "") String mdaCd,
+			@SessionAttribute(name = "mfpUser") MFPUser mfpUser) {
+		String sqlName = getKPIQueryFilePath(AppConstants.SQL_LIST_DEALERS);
+		ListService<ListDealer> service = new ListService<ListDealer>();
+		List<ListDealer> retRows = null;
+		DealerFilter df = new DealerFilter(mfpUser, null, rgnCd, zoneCd, districtCd, mdaCd);
+		try {
+			retRows = service.getListData(sqlName, ListDealer.class, df);
+		} catch (InstantiationException | IllegalAccessException | ParseException e) {
+			log.error("ERROR retrieving list of Dealers:", e);
+		}
+		return AbstractService.httpPostSuccess(retRows, "Success");
+	}
 
-    //
-    @PostMapping("/ListDistricts")
-    public CommonResponse<List<ListDistrict>> listDistricts(
-            @RequestParam(value = "rgnCd", defaultValue = "") String rgnCd,
-            @RequestParam(value = "zoneCd", defaultValue = "") String zoneCd,
-            @SessionAttribute(name = "mfpUser") MFPUser mfpUser) {
-        String sqlName = getKPIQueryFilePath(AppConstants.SQL_LIST_DISTRICTS);
-        ListService<ListDistrict> service = new ListService<ListDistrict>();
-        List<ListDistrict> retRows = null;
-        DealerFilter df = new DealerFilter(mfpUser, null, rgnCd, zoneCd, null, null);
-        try {
-            retRows = service.getListData(sqlName, ListDistrict.class, df, rgnCd, zoneCd);
-        } catch (InstantiationException | IllegalAccessException | ParseException e) {
-            log.error("ERROR retrieving list of Districts:", e);
-        }
-        return AbstractService.httpPostSuccess(retRows, "Success");
-    }
+	//
+	@PostMapping("/ListDistricts")
+	public CommonResponse<List<ListDistrict>> listDistricts(
+			@RequestParam(value = "rgnCd", defaultValue = "") String rgnCd,
+			@RequestParam(value = "zoneCd", defaultValue = "") String zoneCd,
+			@SessionAttribute(name = "mfpUser") MFPUser mfpUser) {
+		String sqlName = getKPIQueryFilePath(AppConstants.SQL_LIST_DISTRICTS);
+		ListService<ListDistrict> service = new ListService<ListDistrict>();
+		List<ListDistrict> retRows = null;
+		DealerFilter df = new DealerFilter(mfpUser, null, rgnCd, zoneCd, null, null);
+		try {
+			retRows = service.getListData(sqlName, ListDistrict.class, df, rgnCd, zoneCd);
+		} catch (InstantiationException | IllegalAccessException | ParseException e) {
+			log.error("ERROR retrieving list of Districts:", e);
+		}
+		return AbstractService.httpPostSuccess(retRows, "Success");
+	}
 
-    //
-    @PostMapping("/ListZones")
-    public CommonResponse<List<ListZone>> listZones(@RequestParam(value = "rgnCd", defaultValue = "") String rgnCd,
-                                                    @SessionAttribute(name = "mfpUser") MFPUser mfpUser) {
-        String sqlName = getKPIQueryFilePath(AppConstants.SQL_LIST_ZONES);
-        ListService<ListZone> service = new ListService<ListZone>();
-        List<ListZone> retRows = null;
-        DealerFilter df = new DealerFilter(mfpUser, null, rgnCd, null, null, null);
-        try {
-            retRows = service.getListData(sqlName, ListZone.class, df, rgnCd);
-        } catch (InstantiationException | IllegalAccessException | ParseException e) {
-            log.error("ERROR retrieving list of Zones:", e);
-        }
-        return AbstractService.httpPostSuccess(retRows, "Success");
-    }
+	//
+	@PostMapping("/ListZones")
+	public CommonResponse<List<ListZone>> listZones(@RequestParam(value = "rgnCd", defaultValue = "") String rgnCd,
+			@SessionAttribute(name = "mfpUser") MFPUser mfpUser) {
+		String sqlName = getKPIQueryFilePath(AppConstants.SQL_LIST_ZONES);
+		ListService<ListZone> service = new ListService<ListZone>();
+		List<ListZone> retRows = null;
+		DealerFilter df = new DealerFilter(mfpUser, null, rgnCd, null, null, null);
+		try {
+			retRows = service.getListData(sqlName, ListZone.class, df, rgnCd);
+		} catch (InstantiationException | IllegalAccessException | ParseException e) {
+			log.error("ERROR retrieving list of Zones:", e);
+		}
+		return AbstractService.httpPostSuccess(retRows, "Success");
+	}
 
-    //
-    @PostMapping("/ListRegions")
-    public CommonResponse<List<ListRegion>> listRegions(@SessionAttribute(name = "mfpUser") MFPUser mfpUser) {
-        String sqlName = getKPIQueryFilePath(AppConstants.SQL_LIST_REGIONS);
-        ListService<ListRegion> service = new ListService<ListRegion>();
-        List<ListRegion> retRows = null;
-        DealerFilter df = new DealerFilter(mfpUser, null, null, null, null, null);
-        try {
-            retRows = service.getListData(sqlName, ListRegion.class, df);
-        } catch (InstantiationException | IllegalAccessException | ParseException e) {
-            log.error("ERROR retrieving list of Regions:", e);
-        }
-        return AbstractService.httpPostSuccess(retRows, "Success");
-    }
+	//
+	@PostMapping("/ListRegions")
+	public CommonResponse<List<ListRegion>> listRegions(@SessionAttribute(name = "mfpUser") MFPUser mfpUser) {
+		String sqlName = getKPIQueryFilePath(AppConstants.SQL_LIST_REGIONS);
+		ListService<ListRegion> service = new ListService<ListRegion>();
+		List<ListRegion> retRows = null;
+		DealerFilter df = new DealerFilter(mfpUser, null, null, null, null, null);
+		try {
+			retRows = service.getListData(sqlName, ListRegion.class, df);
+		} catch (InstantiationException | IllegalAccessException | ParseException e) {
+			log.error("ERROR retrieving list of Regions:", e);
+		}
+		return AbstractService.httpPostSuccess(retRows, "Success");
+	}
 
-    //
-    @PostMapping("/ListMarkets")
-    public CommonResponse<List<ListMarket>> listMarkets(@SessionAttribute(name = "mfpUser") MFPUser mfpUser) {
-        String sqlName = getKPIQueryFilePath(AppConstants.SQL_LIST_MARKETS);
-        ListService<ListMarket> service = new ListService<ListMarket>();
-        List<ListMarket> retRows = null;
-        DealerFilter df = new DealerFilter(mfpUser, null, null, null, null, null);
-        try {
-            retRows = service.getListData(sqlName, ListMarket.class, df);
-        } catch (InstantiationException | IllegalAccessException | ParseException e) {
-            log.error("ERROR retrieving list of Markets:", e);
-        }
-        return AbstractService.httpPostSuccess(retRows, "Success");
-    }
+	//
+	@PostMapping("/ListMarkets")
+	public CommonResponse<List<ListMarket>> listMarkets(@SessionAttribute(name = "mfpUser") MFPUser mfpUser) {
+		String sqlName = getKPIQueryFilePath(AppConstants.SQL_LIST_MARKETS);
+		ListService<ListMarket> service = new ListService<ListMarket>();
+		List<ListMarket> retRows = null;
+		DealerFilter df = new DealerFilter(mfpUser, null, null, null, null, null);
+		try {
+			retRows = service.getListData(sqlName, ListMarket.class, df);
+		} catch (InstantiationException | IllegalAccessException | ParseException e) {
+			log.error("ERROR retrieving list of Markets:", e);
+		}
+		return AbstractService.httpPostSuccess(retRows, "Success");
+	}
 
-    //
-    @PostMapping("/ListDealerEmployees")
-    public CommonResponse<List<ListApprover>> listDealerEmployees(@RequestParam(value = "rgnCd", defaultValue = "") String rgnCd,
-                                                                  @RequestParam(value = "zoneCd", defaultValue = "") String zoneCd,
-                                                                  @RequestParam(value = "districtCd", defaultValue = "") String districtCd,
-                                                                  @RequestParam(value = "mdaCd", defaultValue = "") String mdaCd,
-                                                                  @RequestParam(value = "dlrCd", defaultValue = "") String dlrCd,
-                                                                  @SessionAttribute(name = "mfpUser") MFPUser mfpUser) {
-        String sqlName = getKPIQueryFilePath(AppConstants.SQL_LIST_DEALER_EMPLOYEES);
-        MMAListService<ListApprover> service = new MMAListService<ListApprover>();
-        List<ListApprover> retRows = null;
-        DealerFilter df = new DealerFilter(mfpUser, dlrCd, rgnCd, zoneCd, districtCd, mdaCd);
-        try {
-            retRows = service.getListData(sqlName, ListApprover.class, df, df.getDlrCd());
-        } catch (InstantiationException | IllegalAccessException | ParseException e) {
-            log.error("ERROR retrieving list of Employees:", e);
-        }
-        return AbstractService.httpPostSuccess(retRows, "Success");
-    }
+	//
+	@PostMapping("/ListDealerEmployees")
+	public CommonResponse<List<ListApprover>> listDealerEmployees(
+			@RequestParam(value = "rgnCd", defaultValue = "") String rgnCd,
+			@RequestParam(value = "zoneCd", defaultValue = "") String zoneCd,
+			@RequestParam(value = "districtCd", defaultValue = "") String districtCd,
+			@RequestParam(value = "mdaCd", defaultValue = "") String mdaCd,
+			@RequestParam(value = "dlrCd", defaultValue = "") String dlrCd,
+			@SessionAttribute(name = "mfpUser") MFPUser mfpUser) {
+		String sqlName = getKPIQueryFilePath(AppConstants.SQL_LIST_DEALER_EMPLOYEES);
+		MMAListService<ListApprover> service = new MMAListService<ListApprover>();
+		List<ListApprover> retRows = null;
+		DealerFilter df = new DealerFilter(mfpUser, dlrCd, rgnCd, zoneCd, districtCd, mdaCd);
+		try {
+			retRows = service.getListData(sqlName, ListApprover.class, df, df.getDlrCd());
+		} catch (InstantiationException | IllegalAccessException | ParseException e) {
+			log.error("ERROR retrieving list of Employees:", e);
+		}
+		return AbstractService.httpPostSuccess(retRows, "Success");
+	}
 
-    //
-    @PostMapping("/ListReviewerEmployees")
-    public CommonResponse<List<ListApprover>> listReviewerEmployees(@RequestParam(value = "rgnCd", defaultValue = "") String rgnCd,
-                                                                    @RequestParam(value = "zoneCd", defaultValue = "") String zoneCd,
-                                                                    @RequestParam(value = "districtCd", defaultValue = "") String districtCd,
-                                                                    @RequestParam(value = "mdaCd", defaultValue = "") String mdaCd,
-                                                                    @RequestParam(value = "dlrCd", defaultValue = "") String dlrCd,
-                                                                    @SessionAttribute(name = "mfpUser") MFPUser mfpUser) {
-        String sqlName = getKPIQueryFilePath(AppConstants.SQL_LIST_REVIEWER_EMPLOYEES);
-        MMAListService<ListApprover> service = new MMAListService<ListApprover>();
-        List<ListApprover> retRows = null;
-        DealerFilter df = new DealerFilter(mfpUser, dlrCd, rgnCd, zoneCd, districtCd, mdaCd);
-        try {
-            retRows = service.getListData(sqlName, ListApprover.class, df, mfpUser.getRgnCd());
-        } catch (InstantiationException | IllegalAccessException | ParseException e) {
-            log.error("ERROR retrieving list of Employees:", e);
-        }
-        return AbstractService.httpPostSuccess(retRows, "Success");
-    }
+	//
+	@PostMapping("/ListReviewerEmployees")
+	public CommonResponse<List<ListApprover>> listReviewerEmployees(
+			@RequestParam(value = "rgnCd", defaultValue = "") String rgnCd,
+			@RequestParam(value = "zoneCd", defaultValue = "") String zoneCd,
+			@RequestParam(value = "districtCd", defaultValue = "") String districtCd,
+			@RequestParam(value = "mdaCd", defaultValue = "") String mdaCd,
+			@RequestParam(value = "dlrCd", defaultValue = "") String dlrCd,
+			@SessionAttribute(name = "mfpUser") MFPUser mfpUser) {
+		String sqlName = getKPIQueryFilePath(AppConstants.SQL_LIST_REVIEWER_EMPLOYEES);
+		MMAListService<ListApprover> service = new MMAListService<ListApprover>();
+		List<ListApprover> retRows = null;
+		DealerFilter df = new DealerFilter(mfpUser, dlrCd, rgnCd, zoneCd, districtCd, mdaCd);
+		try {
+			retRows = service.getListData(sqlName, ListApprover.class, df, mfpUser.getRgnCd());
+		} catch (InstantiationException | IllegalAccessException | ParseException e) {
+			log.error("ERROR retrieving list of Employees:", e);
+		}
+		return AbstractService.httpPostSuccess(retRows, "Success");
+	}
 
-    //
-    @PostMapping("/ListCorporateEmployees")
-    public CommonResponse<List<ListEmployee>> listCorporateEmployees(@RequestParam(value = "rgnCd", defaultValue = "") String rgnCd,
-                                                                     @RequestParam(value = "zoneCd", defaultValue = "") String zoneCd,
-                                                                     @RequestParam(value = "districtCd", defaultValue = "") String districtCd,
-                                                                     @RequestParam(value = "mdaCd", defaultValue = "") String mdaCd,
-                                                                     @RequestParam(value = "dlrCd", defaultValue = "") String dlrCd,
-                                                                     @SessionAttribute(name = "mfpUser") MFPUser mfpUser) {
-        String sqlName = getKPIQueryFilePath(AppConstants.SQL_LIST_DEALER_EMPLOYEES);
-        ListService<ListEmployee> service = new ListService<ListEmployee>();
-        List<ListEmployee> retRows = null;
-        DealerFilter df = new DealerFilter(mfpUser, dlrCd, rgnCd, zoneCd, districtCd, mdaCd);
-        try {
-            retRows = service.getListData(sqlName, ListEmployee.class, df);
-        } catch (InstantiationException | IllegalAccessException | ParseException e) {
-            log.error("ERROR retrieving list of Employees:", e);
-        }
-        return AbstractService.httpPostSuccess(retRows, "Success");
-    }
-
+	//
+	@PostMapping("/ListCorporateEmployees")
+	public CommonResponse<List<ListApprover>> listCorporateEmployees(
+			@RequestParam(value = "rgnCd", defaultValue = "") String rgnCd,
+			@RequestParam(value = "zoneCd", defaultValue = "") String zoneCd,
+			@RequestParam(value = "districtCd", defaultValue = "") String districtCd,
+			@RequestParam(value = "mdaCd", defaultValue = "") String mdaCd,
+			@RequestParam(value = "dlrCd", defaultValue = "") String dlrCd,
+			@SessionAttribute(name = "mfpUser") MFPUser mfpUser) {
+		String sqlName = getKPIQueryFilePath(AppConstants.SQL_LIST_DEALER_EMPLOYEES);
+		MMAListService<ListApprover> service = new MMAListService<ListApprover>();
+		List<ListApprover> retRows = null;
+		DealerFilter df = new DealerFilter(mfpUser, dlrCd, rgnCd, zoneCd, districtCd, mdaCd);
+		try {
+			retRows = service.getListData(sqlName, ListApprover.class, df, mfpUser.getRgnCd());
+		} catch (InstantiationException | IllegalAccessException | ParseException e) {
+			log.error("ERROR retrieving list of Employees:", e);
+		}
+		return AbstractService.httpPostSuccess(retRows, "Success");
+	}
 
 }
