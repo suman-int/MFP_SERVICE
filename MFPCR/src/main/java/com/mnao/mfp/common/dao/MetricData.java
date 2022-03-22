5255346a5dc13c68be6ee01b35955d84bd9e330f
smukher1 <smukher1@mazdausa.com>
2022-03-22 20:20:58 +0530

    Null replaced with ''

--- MFPCR/src/main/java/com/mnao/mfp/common/dao/MetricData.java
+++ MFPCR/src/main/java/com/mnao/mfp/common/dao/MetricData.java
@@ -4,25 +4,26 @@ import java.lang.reflect.Field;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-public abstract  class MetricData  {
-	public abstract void setResultSetRow(ResultSet rs) throws SQLException ;
-	protected void setFieldValue(String fldName, ResultSet rs, int  colIdx ) throws SQLException {
+public abstract class MetricData {
+	public abstract void setResultSetRow(ResultSet rs) throws SQLException;
+
+	protected void setFieldValue(String fldName, ResultSet rs, int colIdx) throws SQLException {
 		String val = rs.getString(colIdx);
-		if( val != null && val.trim().length() > 0 ) {
-			try {
-				val = val.trim();
-				Field fld = this.getClass().getDeclaredField(fldName);
-				fld.setAccessible(true);
-				fld.set(this, val);
-			} catch (NoSuchFieldException e) {
-				e.printStackTrace();
-			} catch (SecurityException e) {
-				e.printStackTrace();
-			} catch (IllegalArgumentException e) {
-				e.printStackTrace();
-			} catch (IllegalAccessException e) {
-				e.printStackTrace();
-			}
+		if (val == null)
+			val = "";
+		try {
+			val = val.trim();
+			Field fld = this.getClass().getDeclaredField(fldName);
+			fld.setAccessible(true);
+			fld.set(this, val);
+		} catch (NoSuchFieldException e) {
+			e.printStackTrace();
+		} catch (SecurityException e) {
+			e.printStackTrace();
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace();
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
 		}
 	}
 }
