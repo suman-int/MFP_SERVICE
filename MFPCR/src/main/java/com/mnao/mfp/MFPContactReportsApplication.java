b6794c5cae2ee79d78354222c05f4f440dc4f337
sandip-mukherjee <sandip@simplesolutio.com>
2022-04-06 14:18:14 +0530

    feature/wsl_integration

--- MFPCR/src/main/java/com/mnao/mfp/MFPContactReportsApplication.java
+++ MFPCR/src/main/java/com/mnao/mfp/MFPContactReportsApplication.java
@@ -1,13 +1,16 @@
 package com.mnao.mfp;
 
 import org.springframework.boot.SpringApplication;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 import com.mnao.mfp.common.annotation.MFPCommonService;
 import com.mnao.mfp.list.annotation.MFPListService;
 import com.mnao.mfp.list.dao.ListDealer;
 import com.mnao.mfp.user.annotation.MFPUserService;
 
+@ServletComponentScan
 @SpringBootApplication
 //@MFPUserService
 //@MFPListService
---
+++ MFPCR/src/main/java/com/mnao/mfp/login/LoggedInUser.java
@@ -0,0 +1,62 @@
+package com.mnao.mfp.login;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mnao.mfp.common.util.AppConstants;
+import com.mnao.mfp.common.util.Utils;
+
+@WebServlet(urlPatterns = "/login/*", loadOnStartup = 1)
+public class LoggedInUser extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private static final String USERID_REQUEST_HEADER = "IV-USER";
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("Login Servlet");
+		String defUrl = "https://portaltest.mazdausa.com/m320/mfpwebui/index.html";
+		String mfpUrl = Utils.getAppProperty(AppConstants.CR_URL_KEY);
+		if (mfpUrl == null || mfpUrl.trim().length() == 0)
+			mfpUrl = defUrl;
+		String userID = req.getHeader(USERID_REQUEST_HEADER);
+		if (userID != null && userID.trim().length() > 0) {
+			resp.setContentType("text/html");
+			PrintWriter out = resp.getWriter();
+			printHTML(out, mfpUrl, userID);
+			resp.setStatus(HttpServletResponse.SC_OK);
+			Cookie userCookie = new Cookie("IV-USER", userID);
+			resp.addCookie(userCookie);
+		} else {
+			resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+		}
+	}
+
+	private void printHTML(PrintWriter out, String mfpUiUrl, String userID) {
+		String url = mfpUiUrl;
+		//url += "?" + userID;
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Contact Reports</title>");
+		out.println("<script type=\"text/javascript\">");
+		out.println("document.cookie = \"USER="+ userID + "; SameSite=None; path=/;");
+		out.println("window.location=\"" + url + "\";");
+		out.println("</script>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+
+}
