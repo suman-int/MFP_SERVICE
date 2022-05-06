e9c5cb15ca18df6f667d270ab8433b04bfd9e79e
sandip-mukherjee <sandip@simplesolutio.com>
2022-05-06 17:51:07 +0530

    Changes from EntGit

--- MFPCR/src/main/java/com/mnao/mfp/user/interceptors/MFPRequestInterceptor.java
+++ MFPCR/src/main/java/com/mnao/mfp/user/interceptors/MFPRequestInterceptor.java
@@ -1,19 +1,15 @@
 package com.mnao.mfp.user.interceptors;
 
 import com.mnao.mfp.common.util.AppConstants;
-import com.mnao.mfp.common.util.NullCheck;
 import com.mnao.mfp.common.util.Utils;
-import com.mnao.mfp.config.ApplicationProperties;
-import com.mnao.mfp.config.CrConfiguration;
 import com.mnao.mfp.list.emp.AllEmployeesCache;
 import com.mnao.mfp.user.dao.MFPUser;
 import com.mnao.mfp.user.service.UserDetailsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.env.Environment;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.WebApplicationContextUtils;
@@ -23,118 +19,118 @@ import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-import javax.validation.constraints.NotNull;
-
 import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Properties;
 
 @Configuration
 @Component
+@Profile("test")
 public class MFPRequestInterceptor implements HandlerInterceptor {
-	
-	@Autowired
-	private ApplicationProperties properties;
 
-	@Autowired
-	private CrConfiguration configuration;
+	@Value("${spring.profiles.active}")
+	private String activeProfile;
 
-    private static final Logger log = LoggerFactory.getLogger(MFPRequestInterceptor.class);
-    private static final String USERID_REQUEST_HEADER = "IV-USER";
-    private final boolean useDBDomain;
+	private static final Logger log = LoggerFactory.getLogger(MFPRequestInterceptor.class);
+	private static final String USERID_REQUEST_HEADER = "IV-USER";
+	private final boolean useDBDomain;
 
-    public MFPRequestInterceptor() {
-        String useDB = Utils.getAppProperty(AppConstants.EMP_USE_DB_RGN_ZONE_DSTR, "false");
-        useDBDomain = useDB.equalsIgnoreCase("true");
-        log.debug("Interceptor Created. Use DB for USer Domain: {}", useDBDomain);
-    }
+	public MFPRequestInterceptor() {
+		String useDB = Utils.getAppProperty(AppConstants.EMP_USE_DB_RGN_ZONE_DSTR, "false");
+		useDBDomain = useDB.equalsIgnoreCase("true");
+		log.debug("Interceptor Created. Use DB for USer Domain: {}", useDBDomain);
+	}
 
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg2) throws Exception {
-        boolean rv = true;
-        String userID = request.getHeader(USERID_REQUEST_HEADER);
-        log.info("Active Env : {}", configuration);
-        if ("DEV".equalsIgnoreCase(configuration.getActiveProfile()) || validateToken(request, response)) {
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg2) throws Exception {
+		boolean rv = true;
+		String userID = request.getHeader(USERID_REQUEST_HEADER);
 
-            log.debug("UserID= {}", userID);
-            if (userID == null || userID.trim().length() == 0) {
-                response.sendError(401, "UNAUTHORISED");
-                log.error("Unauthorised Access");
-                rv = false;
-            } else {
-                UserDetailsService ud = new UserDetailsService();
-                MFPUser u = ud.getMFPUser(userID);
-                if (u == null) {
-                    response.sendError(401, "UNAUTHORISED");
-                    log.error("Unauthorised Access");
-                    rv = false;
-                } else {
-                    if (useDBDomain) {
-                        u.setUseDBDomain(true);
-                        try {
-                            ServletContext servletContext = request.getServletContext();
-                            WebApplicationContext wac = WebApplicationContextUtils
-                                    .getRequiredWebApplicationContext(servletContext);
-                            AllEmployeesCache allEmployeesCache = wac.getBean(AllEmployeesCache.class);
-                            if (allEmployeesCache != null) {
-                                allEmployeesCache.updateDomain(u);
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    HttpSession session = request.getSession();
-                    session.setAttribute("mfpUser", u);
-                }
-            }
+		if (validateToken(request, response)) {
 
-        } else {
-            response.sendError(401, "UNAUTHORISED");
-            log.error("Unauthorised Access");
-            rv = false;
-        }
+			log.debug("UserID= {}", userID);
+			if (userID == null || userID.trim().length() == 0) {
+				response.sendError(401, "UNAUTHORISED");
+				log.error("Unauthorised Access");
+				rv = false;
+			} else {
+				UserDetailsService ud = new UserDetailsService();
+				MFPUser u = ud.getMFPUser(userID);
+				if (u == null) {
+					response.sendError(401, "UNAUTHORISED");
+					log.error("Unauthorised Access");
+					rv = false;
+				} else {
+					if (useDBDomain) {
+						u.setUseDBDomain(true);
+						try {
+							ServletContext servletContext = request.getServletContext();
+							WebApplicationContext wac = WebApplicationContextUtils
+									.getRequiredWebApplicationContext(servletContext);
+							AllEmployeesCache allEmployeesCache = wac.getBean(AllEmployeesCache.class);
+							if (allEmployeesCache != null) {
+								allEmployeesCache.updateDomain(u);
+							}
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+					HttpSession session = request.getSession();
+					session.setAttribute("mfpUser", u);
+				}
+			}
 
-        return rv;
-    }
+		} else {
+			response.sendError(401, "UNAUTHORISED");
+			log.error("Unauthorised Access");
+			rv = false;
+		}
 
-    public boolean validateToken(HttpServletRequest request, HttpServletResponse response) {
+		return rv;
+	}
 
-        boolean validateFlag = false;
-        HttpURLConnection con = null;
+	public boolean validateToken(HttpServletRequest request, HttpServletResponse response) {
 
-        try (InputStream ist = Utils.class.getResourceAsStream("/wslusersvc-test.properties")) {
-            Properties wslProperties = new Properties();
-            wslProperties.load(ist);
+		boolean validateFlag = false;
+		HttpURLConnection con = null;
 
-            URL url = new URL(wslProperties.getProperty("AUTH_SVC_URL"));
-            con = (HttpURLConnection) url.openConnection();
-            con.setRequestMethod("GET");
-            con.setRequestProperty(wslProperties.getProperty("AUTH_RS_SEC_HDR_TOKEN_NAME"),
-                    request.getHeader(AppConstants.RS_SEC_HDR_TOKEN_NAME));
-            con.setRequestProperty(wslProperties.getProperty("AUTH_RS_SEC_HDR_IV_NAME"),
-                    request.getHeader(AppConstants.RS_SEC_HDR_IV_NAME));
-            con.setRequestProperty(wslProperties.getProperty("AUTH_RS_SEC_HDR_VENDOR_ID"),
-                    request.getHeader(AppConstants.RS_SEC_HDR_VENDOR_ID));
-            con.setUseCaches(false);
-            con.setDoInput(true);
-            con.setDoOutput(true);
+		/*
+		 * try (InputStream ist =
+		 * Utils.class.getResourceAsStream("/wslusersvc-test.properties")) { Properties
+		 * wslProperties = new Properties(); wslProperties.load(ist);
+		 */
 
-            int statusCode = con.getResponseCode();
-            if (statusCode == 200) {
-                validateFlag = true;
-            }
+		try {
+			Properties wslProperties = Utils.getWslProperties();
 
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            if (con != null) {
-                con.disconnect();
-            }
-        }
+			URL url = new URL(wslProperties.getProperty("AUTH_SVC_URL"));
+			con = (HttpURLConnection) url.openConnection();
+			con.setRequestMethod("GET");
+			con.setRequestProperty(wslProperties.getProperty("AUTH_RS_SEC_HDR_TOKEN_NAME"),
+					request.getHeader(AppConstants.RS_SEC_HDR_TOKEN_NAME));
+			con.setRequestProperty(wslProperties.getProperty("AUTH_RS_SEC_HDR_IV_NAME"),
+					request.getHeader(AppConstants.RS_SEC_HDR_IV_NAME));
+			con.setRequestProperty(wslProperties.getProperty("AUTH_RS_SEC_HDR_VENDOR_ID"),
+					request.getHeader(AppConstants.RS_SEC_HDR_VENDOR_ID));
+			con.setUseCaches(false);
+			con.setDoInput(true);
+			con.setDoOutput(true);
 
-        return validateFlag;
-    }
+			int statusCode = con.getResponseCode();
+			if (statusCode == 200) {
+				validateFlag = true;
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (con != null) {
+				con.disconnect();
+			}
+		}
+
+		return validateFlag;
+	}
 
 }
