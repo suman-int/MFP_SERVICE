41b044bd36732aa3eae331c89038f5954f3e40bd
sandip-mukherjee <sandip@simplesolutio.com>
2022-05-19 01:52:59 +0530

    DB props removed

--- MFPCR/src/main/java/com/mnao/mfp/common/service/AppPropertiesService.java
+++ MFPCR/src/main/java/com/mnao/mfp/common/service/AppPropertiesService.java
@@ -45,7 +45,8 @@ public class AppPropertiesService {
 			String mfpProfName = getPropertiesLocation() + "/" + AppConstants.MFP_PROPS_NAME + ".properties";
 			String[] prof = env.getActiveProfiles();
 			if (prof != null && prof.length > 0) {
-				mfpProfName = getPropertiesLocation() + "/" + AppConstants.MFP_PROPS_NAME + "-" + prof[0] + ".properties";
+				mfpProfName = getPropertiesLocation() + "/" + AppConstants.MFP_PROPS_NAME + "-" + prof[0]
+						+ ".properties";
 			}
 			log.debug("Reading AppProperties From:" + mfpProfName);
 			try (InputStream is = Utils.class.getResourceAsStream(mfpProfName)) {
@@ -60,12 +61,15 @@ public class AppPropertiesService {
 	}
 
 	private void updateAppPropsWithEnv() {
-	    @SuppressWarnings("unchecked")
-		Enumeration<String> enums = (Enumeration<String>) appProps.propertyNames();
-	    while (enums.hasMoreElements()) {
-			String key = enums.nextElement();
+		@SuppressWarnings("unchecked")
+		String[] dbprops = { "database.jdbc.mfp.url", "database.jdbc.mfp.user", "database.jdbc.mfp.pass",
+				"database.jdbc.mfp.schema", "database.jdbc.cr.url", "database.jdbc.cr.user", "database.jdbc.cr.pass",
+				"database.jdbc.cr.schema", "database.jdbc.mma.url", "database.jdbc.mma.user", "database.jdbc.mma.pass",
+				"database.jdbc.mma.schema", "database.jdbc.edw.url", "database.jdbc.edw.user", "database.jdbc.edw.pass",
+				"database.jdbc.edw.schema" };
+		for (String key : dbprops) {
 			String val = env.getProperty(key);
-			if( val != null && !val.equalsIgnoreCase("#{null}")) {
+			if (val != null && !val.equalsIgnoreCase("#{null}")) {
 				appProps.setProperty(key, val);
 				log.debug("Updated Property Value of " + key + " with " + val + " from Environment.");
 			}
@@ -77,7 +81,8 @@ public class AppPropertiesService {
 			String wslPropName = getPropertiesLocation() + "/" + AppConstants.WSL_PROPS_NAME + ".properties";
 			String[] prof = env.getActiveProfiles();
 			if (prof != null && prof.length > 0) {
-				wslPropName = getPropertiesLocation() + "/" + AppConstants.WSL_PROPS_NAME + "-" + prof[0] + ".properties";
+				wslPropName = getPropertiesLocation() + "/" + AppConstants.WSL_PROPS_NAME + "-" + prof[0]
+						+ ".properties";
 			}
 			log.debug("Reading WSLUserSvcProperties From:" + wslPropName);
 			try (InputStream is = Utils.class.getResourceAsStream(wslPropName)) {
