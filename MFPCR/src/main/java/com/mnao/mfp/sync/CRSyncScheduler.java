96a83b5c9904e8a5f791fcb9bc4d6ff5dc8d857b
sandip-mukherjee <sandip@simplesolutio.com>
2022-05-20 21:17:27 +0530

    Property files changed.

--- MFPCR/src/main/java/com/mnao/mfp/sync/CRSyncScheduler.java
+++ MFPCR/src/main/java/com/mnao/mfp/sync/CRSyncScheduler.java
@@ -1,12 +1,34 @@
 package com.mnao.mfp.sync;
 
+import java.time.LocalDateTime;
+
+import javax.annotation.PostConstruct;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.support.CronExpression;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CRSyncScheduler {
-	// run during the first second every minute every hour every day of month
-	// //every month every day of week
+	//
+	private static final Logger log = LoggerFactory.getLogger(CRSyncScheduler.class);
+
+	@Value("${edw.sync.schedule.cron}")
+	private String CronSetting;
+	
+    @PostConstruct
+    public void init() {
+        //Update: Resolve compile time error for static method `parse`
+        CronExpression cronTrigger = CronExpression.parse(CronSetting);
+
+        LocalDateTime next = cronTrigger.next(LocalDateTime.now());
+
+        log.debug("Next Sync Execution Time: " + next);
+    }
+
 	@Scheduled(cron = "${edw.sync.schedule.cron}")
 	public void ExecEDWSync() {
 		CRSyncFromEDW crSync = new CRSyncFromEDW();
