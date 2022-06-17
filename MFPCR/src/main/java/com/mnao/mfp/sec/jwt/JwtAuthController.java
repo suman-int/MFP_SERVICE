cb917c14d7cf20aa88be7cac84721e25b97d0aa7
sandip-mukherjee <sandip@simplesolutio.com>
2022-06-17 17:15:38 +0530

    Authorize-with-UserInfo

--- MFPCR/src/main/java/com/mnao/mfp/sec/jwt/JwtAuthController.java
+++ MFPCR/src/main/java/com/mnao/mfp/sec/jwt/JwtAuthController.java
@@ -1,8 +1,5 @@
 package com.mnao.mfp.sec.jwt;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
 
@@ -10,19 +7,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
 import com.mnao.mfp.common.util.AppConstants;
-import com.mnao.mfp.list.cache.AllEmployeesCache;
 import com.mnao.mfp.user.controller.MFPUserController;
 import com.mnao.mfp.user.dao.MFPUser;
-import com.mnao.mfp.user.service.UserDetailsService;
 
 @RestController
 @CrossOrigin(origins = "*")
@@ -34,7 +26,7 @@ public class JwtAuthController {
 	JwtTokenUtil jwtTokenUtil;
 
 	//
-	//@PostMapping("/Authorize")
+	// @PostMapping("/Authorize")
 	@RequestMapping(value = "/Authorize", method = { RequestMethod.GET, RequestMethod.POST })
 	public JwtResponse authorize(@SessionAttribute(name = "mfpUser") MFPUser mfpUser, HttpServletResponse response) {
 		String token = jwtTokenUtil.generateToken(mfpUser);
@@ -45,6 +37,17 @@ public class JwtAuthController {
 		response.addCookie(ck);
 		return new JwtResponse(token);
 	}
+	
 
+	@RequestMapping(value = "/AuthorizeUser", method = { RequestMethod.GET, RequestMethod.POST })
+	public JwtUerTokenResponse authorizeUser(@SessionAttribute(name = "mfpUser") MFPUser mfpUser, HttpServletResponse response) {
+		String token = jwtTokenUtil.generateToken(mfpUser);
+		Cookie ck = new Cookie(AppConstants.AUTH_COOKIE, token);
+		ck.setMaxAge(5 * 60 * 60);
+		ck.setPath("/");
+		ck.setHttpOnly(true);
+		response.addCookie(ck);
+		return new JwtUerTokenResponse(token, mfpUser);
+	}
 
 }
